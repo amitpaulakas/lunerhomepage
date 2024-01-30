fa1a002132d8bc8da27c467a15842a9b25689aa8
amitpaulakas <amitpaulakas@gmail.com>
2024-01-30 21:19:03 +0600

    js exam

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+let namta = 5;
+
+for (let i = 1; i <= 20; i++) {
+    console.log(namta + " x " + i + " = " + "tumer " +  "boyos " + namta * i ) ;
+   
+let age  = i*5;
+
+if (age >= 0 && age <= 15){
+    console.log("tumi akn o boro how ni")
+}
+else if (age >= 15 && age <=20 ){
+    console.log("Taka income korle biye korte paro")
+}
+else if (age >= 21 && age <=35){
+    console.log("biye korte paro")
+}
+else if (age >= 36 && age <=80){
+    console.log("biyer boyos per hoye galo ")
+}
+else if (age >= 80 && age <=100){
+    console.log("caca biye korer chinta chere den")
+}
+
+
+}
--- js/main.js
+++
@@ -1,5 +0,0 @@
-let namta = 5;
-
-for (let i = 1; i<= 20; i++){
-    console.log(namta ="x" + i "=" + namta *i) ;
-}
